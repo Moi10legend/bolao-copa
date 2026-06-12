ef376bc273ab4bef15cdbac15c53651e362b2859
Moi10legend <moisesflopesf@gmail.com>
2026-06-12 10:57:55 -0300

    fix: hardcode da URL da API direto no código

--- frontend_bolao/app/lib/api.ts
+++ frontend_bolao/app/lib/api.ts
@@ -1,5 +1,5 @@
 // src/lib/api.ts
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+const API_BASE_URL = "https://bolao-copa-api-90t0.onrender.com"//process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 
 
